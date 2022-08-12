44fd404afbd4af9927068ff5075d8f7186051645
Noid <dionisiusgunadi@outlook.com>
2022-08-13 00:57:23 +0700

    apply axios fetch method

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,29 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456", id: 1 },
     { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
     { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 }
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((res) => {
+        setPersons(res.data);
+      })
+      .catch((err) => {
+        console.log(err.response.status);
+      });
+  }, []);
+
   const [filterQuery, setFilterQuery] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
@@ -34,8 +47,8 @@ const App = () => {
       {
         name: newName,
         number: newNumber,
-        id: +new Date()
-      }
+        id: +new Date(),
+      },
     ]);
   };
 
