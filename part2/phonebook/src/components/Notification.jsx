a7b0df996a71109bbb94c1f835390f520102aec6
Noid <dionisiusgunadi@outlook.com>
2022-08-19 21:55:11 +0700

    remove console.log

--- part2/phonebook/src/components/Notification.jsx
+++ part2/phonebook/src/components/Notification.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Notification = ({ message, status }) => {
   const className = `notification ${status}`;
-  console.log(className);
   return <div className={className}>{message}</div>;
 };
 
